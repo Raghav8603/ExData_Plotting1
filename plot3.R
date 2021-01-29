e4aeb7def898fd217a4c77d03dfa403c32ab0f7d
Raghav Vashisht <70436130+Raghav8603@users.noreply.github.com>
2021-01-29 14:58:37 +0530

    Create Plot3 code

---
+++ plot3.R
@@ -0,0 +1,12 @@
+HPC<-read.table("household_power_consumption.txt",sep=";",header = T, na.strings = "?")
+HPC$Date <- as.Date(HPC$Date, format = "%d/%m/%Y")
+HPC<-subset(HPC, HPC$Date == "2007-02-01"| HPC$Date =="2007-02-02")
+HPC$DateTime <- strptime(paste(HPC$Date, HPC$Time), "%Y-%m-%d %H:%M:%S")
+with(HPC, {
+  plot(DateTime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+  lines(DateTime, Sub_metering_2, col='Red')
+  lines(DateTime, Sub_metering_3, col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, y.intersp = 0.5, cex = 0.9, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
